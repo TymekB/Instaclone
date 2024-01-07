f07d6f6458abe9bc3aa9808d12a3bb191874b508
Tymoteusz Baran <tymoteusz.baran@morele.net>
2024-01-07 14:12:00 +0100

    feat(component): add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import LoginPage from "./components/LoginPage";
+import LoginPage from "./components/LoginPage/LoginPage";
+import React from "react";
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Home from "./components/Home/Home";
 
-function App() {
-    return (
-        <LoginPage/>
-    );
-
-
+class App extends React.Component
+{
+    render()
+    {
+        return (
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<LoginPage />}></Route>
+                    <Route path="/home" element={<Home />} />
+                </Routes>
+            </BrowserRouter>
+        );
+    }
 }
 export default App;
